86ecda962d3d29a32ed3cda44cdaa98a0389b6ff
Ben Parman <benparman@gmail.com>
2018-01-22 15:20:51 -0700

    making sure everything is up to date

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
-'use strict'
+'use strict';
 
 //Initializes app once document is loaded
 $(document).ready(function() {
   getSearchTerms();
-})
+});
+
+//The above document function is depricated - use this shorthand format instead
+// $(function() {
+//   getSearchTerms();
+// });
 
 //Store's information about current application state
-let STORE = {
+const STORE = {
   vidThumbs: [],
   videoURLS: []
-}
+};
 
 //Variables to store YouTube API enpdoint and API Key
-const endpoint = "https://www.googleapis.com/youtube/v3/search?";
-const apiKey = "AIzaSyCXea0ie5sLMSUTuJOdNrkXuk-UX61BSmw";
+// const endpoint = "https://www.googleapis.com/youtube/v3/search?";  ****Moved these variables to queryAPI function
+// const apiKey = "AIzaSyCXea0ie5sLMSUTuJOdNrkXuk-UX61BSmw";
 
 //Event Listeners / Handlers
 function getSearchTerms() {
   //Calls 'queryAPI' function to query YouTube API with user's search term and default app parameters (ie maxResults);
     $('#js-vidSearch').submit(function(event) {
-    STORE.vidThumbs = [];
-    STORE.videoURLS = [];
-    event.preventDefault();
-    let searchTerms = $('#searchTerm').val();
-    console.log(`Search Term: ${searchTerms}`);
-    //Calls 'queryAPI' function, passing in the value of 'searchTerm' as the functions 'query' argument
-    //If statement ensures user has entered text.  Renders video thumbnails if so, otherwise renders a message to enter text
-    if (($('#searchTerm').val() !== '')) {
+      resetStore();
+      // STORE.vidThumbs = [];  ***moved these to seperate function
+      // STORE.videoURLS = [];  ***see 'resetStore' function
+      event.preventDefault();
+      let searchTerms = $('#searchTerm').val();
+      console.log(`Search Term: ${searchTerms}`);
+      //Calls 'queryAPI' function, passing in the value of 'searchTerm' as the functions 'query' argument
+      //If statement ensures user has entered text.  Renders video thumbnails if so, otherwise renders a message to enter text
+      if (($('#searchTerm').val() !== '')) {
       queryAPI(searchTerms);
       } else {
         renderTextMessage();
@@ -34,8 +40,16 @@ function getSearchTerms() {
     })
 }
 
+//Function to reset status of STORE
+function resetStore() {
+  STORE.vidThumbs = [];
+  STORE.videoURLS = [];
+}
+
 //API Query Function
 function queryAPI(query) {  //'query' = 'searchTerms' (defined in 'getSearchTerms') which = user's search terms
+  const endpoint = "https://www.googleapis.com/youtube/v3/search?";
+  const apiKey = "AIzaSyCXea0ie5sLMSUTuJOdNrkXuk-UX61BSmw";
   let params = {
     part: 'snippet',
     key: apiKey,
@@ -63,6 +77,7 @@ function sortResults(results) {  //results argument called with JSONdata in quer
 function generateThumbnails() {
   let thumbsList = [];
   for (let i=0; i<STORE.vidThumbs.length; i++) {
+    console.log(i);
   thumbsList.push(`<li><a href="${STORE.videoURLS[i]}"><img src="${STORE.vidThumbs[i]}" alt="video thumbnail"></li>`);
   }
  // console.log(`<ul>${thumbsList.toString().replace(/,/g/'')}</ul>`);
