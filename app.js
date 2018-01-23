50b94f782ac4c199f1493fd51e2623f3a6bf4523
Ben Parman <benparman@gmail.com>
2018-01-22 18:30:41 -0700

    video titles rendering below thumbnails

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ $(document).ready(function() {
 //Store's information about current application state
 const STORE = {
   vidThumbs: [],
-  videoURLS: []
+  videoURLS: [],
+  videoTitles: []
 };
 
 //Variables to store YouTube API enpdoint and API Key
@@ -23,39 +24,41 @@ const STORE = {
 //Event Listeners / Handlers
 function getSearchTerms() {
   //Calls 'queryAPI' function to query YouTube API with user's search term and default app parameters (ie maxResults);
-    $('#js-vidSearch').submit(function(event) {
-      resetStore();
-      // STORE.vidThumbs = [];  ***moved these to seperate function
-      // STORE.videoURLS = [];  ***see 'resetStore' function
-      event.preventDefault();
-      let searchTerms = $('#searchTerm').val();
-      console.log(`Search Term: ${searchTerms}`);
-      //Calls 'queryAPI' function, passing in the value of 'searchTerm' as the functions 'query' argument
-      //If statement ensures user has entered text.  Renders video thumbnails if so, otherwise renders a message to enter text
-      if (($('#searchTerm').val() !== '')) {
+  $('#js-vidSearch').submit(function(event) {
+    resetStore();
+    // STORE.vidThumbs = [];  ***moved these to seperate function
+    // STORE.videoURLS = [];  ***see 'resetStore' function
+    event.preventDefault();
+    let searchTerms = $('#searchTerm').val();
+    console.log(`Search Term: ${searchTerms}`);
+    //Calls 'queryAPI' function, passing in the value of 'searchTerm' as the functions 'query' argument
+    //If statement ensures user has entered text.  Renders video thumbnails if so, otherwise renders a message to enter text
+    if (($('#searchTerm').val() !== '')) {
       queryAPI(searchTerms);
-      } else {
-        renderTextMessage();
-      }
-    })
+      renderResultCountMessage();
+    } else {
+      renderTextMessage();
+    }
+  });
 }
 
 //Function to reset status of STORE
 function resetStore() {
   STORE.vidThumbs = [];
   STORE.videoURLS = [];
+  STORE.videoTitles = [];
 }
 
 //API Query Function
 function queryAPI(query) {  //'query' = 'searchTerms' (defined in 'getSearchTerms') which = user's search terms
-  const endpoint = "https://www.googleapis.com/youtube/v3/search?";
-  const apiKey = "AIzaSyCXea0ie5sLMSUTuJOdNrkXuk-UX61BSmw";
+  const endpoint = 'https://www.googleapis.com/youtube/v3/search?';
+  const apiKey = 'AIzaSyCXea0ie5sLMSUTuJOdNrkXuk-UX61BSmw';
   let params = {
     part: 'snippet',
     key: apiKey,
     q: query,
     maxResults: 5
-  }
+  };
   $.getJSON(endpoint, params, function(JSONdata) {  //This callback function argument works, but do not fully understand how it works
     console.log(JSONdata);
     sortResults(JSONdata);
@@ -67,6 +70,7 @@ function sortResults(results) {  //results argument called with JSONdata in quer
   for (let i=0; i<results.items.length; i++) {
     STORE.vidThumbs.push(results.items[i].snippet.thumbnails.default.url);
     STORE.videoURLS.push('https://www.youtube.com/watch?v='+results.items[i].id.videoId);
+    STORE.videoTitles.push(results.items[i].snippet.title);
   }
   renderThumbnails();
 }
@@ -78,27 +82,36 @@ function generateThumbnails() {
   let thumbsList = [];
   for (let i=0; i<STORE.vidThumbs.length; i++) {
     console.log(i);
-  thumbsList.push(`<li><a href="${STORE.videoURLS[i]}"><img src="${STORE.vidThumbs[i]}" alt="video thumbnail"></li>`);
+    thumbsList.push(`<li><a href="${STORE.videoURLS[i]}"><img src="${STORE.vidThumbs[i]}" alt="video thumbnail">
+      <span class = "captions">${STORE.videoTitles[i].slice(0,15)}...</span></li>`);
   }
- // console.log(`<ul>${thumbsList.toString().replace(/,/g/'')}</ul>`);
-  return `<ul>${thumbsList.toString().replace(/,/g, '')}</ul>`
+  // console.log(`<ul>${thumbsList.toString().replace(/,/g/'')}</ul>`);
+  return `<ul class ="thumbnails">${thumbsList.toString().replace(/,/g, '')}</ul>`;
 }
 
-function generateTextMessage() {
+function generateErrorMessagee() {
   let errorText = '<h3>You did not enter anything to search for!</h3>';
-  return `${errorText}`
+  return `${errorText}`;
+}
+
+function generateResultCountMessage() {
+  let vidCountText = `<h3>Displaying ${STORE.vidThumbs.length} results</h3>`;
+  return vidCountText;
 }
 
 //HTML Renderers
 function renderThumbnails() {
   let thumbView = generateThumbnails();
+  let text = generateResultCountMessage();
+  $('.js-message').html(text);
   $('.js-results').html(thumbView);
 }
 
 function renderTextMessage() {
-  let text = generateTextMessage();
+  let text = generateErrorMessagee();
   $('.js-results').html(text);
 }
+
 
 //what is the format for a youTube query string?
 
